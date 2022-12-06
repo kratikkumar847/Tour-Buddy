743e59c1a862c0a6d753f43a9d9a34ca5e3cfa09
Kratik Kumar <kratikkumar847@gmail.com>
2022-12-06 20:35:55 +0530

    toast notification

--- src/components/ProfilePage.vue
+++ src/components/ProfilePage.vue
@@ -73,6 +73,7 @@ export default {
             loading: false
         };
     },
+   
 
     
 
--- src/components/SignUpPage.vue
+++ src/components/SignUpPage.vue
@@ -1,56 +1,43 @@
 <template>
-  <div class="wrapper">
-    <h2>SignUp</h2>
-    <form action="#">
-      <div class="input-box">
-        <input
-          type="text"
-          placeholder="Enter your name"
-          required
-          v-model="signupDetails.name"
-        />
-      </div>
-      <div class="input-box">
-        <input
-          type="text"
-          placeholder="Enter your UserID"
-          required
-          v-model="signupDetails.userID"
-        />
-      </div>
-      <div class="input-box">
-        <input
-          type="text"
-          placeholder="Enter your email"
-          required
-          v-model="signupDetails.email"
-        />
-      </div>
-      <div class="input-box">
-        <input
-          type="password"
-          placeholder="Create password"
-          required
-          v-model="signupDetails.password"
-        />
-      </div>
-      <div class="input-box">
-        <input type="password" placeholder="Confirm password" required />
-      </div>
 
-      <div class="policy">
-        <input type="checkbox" />
-        <h3>I accept all terms & condition</h3>
-      </div>
-      <div class="input-box button">
-        <input type="Submit" value="Register Now" @click="signup" />
-      </div>
-      <div class="text">
-        <h3>
-          Already have an account? <router-link to="/">Login now</router-link>
-        </h3>
-      </div>
-    </form>
+  <div>
+    <div v-if="loading">
+      <SpinnerPage />
+    </div>
+
+    <div class="wrapper">
+      <h2>SignUp</h2>
+      <form action="#">
+        <div class="input-box">
+          <input type="text" placeholder="Enter your name" required v-model="signupDetails.name" />
+        </div>
+        <div class="input-box">
+          <input type="text" placeholder="Enter your UserID" required v-model="signupDetails.userID" />
+        </div>
+        <div class="input-box">
+          <input type="text" placeholder="Enter your email" required v-model="signupDetails.email" />
+        </div>
+        <div class="input-box">
+          <input type="password" placeholder="Create password" required v-model="signupDetails.password" />
+        </div>
+        <div class="input-box">
+          <input type="password" placeholder="Confirm password" required />
+        </div>
+
+        <div class="policy">
+          <input type="checkbox" />
+          <h3>I accept all terms & condition</h3>
+        </div>
+        <div class="input-box button">
+          <input type="Submit" value="Register Now" @click="signup" />
+        </div>
+        <div class="text">
+          <h3>
+            Already have an account? <router-link to="/">Login now</router-link>
+          </h3>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -58,6 +45,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 import Config from "@/config";
+import SpinnerPage from '../components/SpinnerPage.vue'
 
 Vue.use(axios)
 
@@ -65,6 +53,7 @@ export default {
   name: "SignUpPage",
   data() {
     return {
+      loading : false,
       signupDetails: {
         name: "",
         userID: "",
@@ -73,29 +62,33 @@ export default {
       },
     };
   },
+  components: {
+        SpinnerPage,
+    },
   methods: {
-     signup(e) {
+    signup(e) {
+      this.loading = true;
       const newSignUp = {
         name: this.signupDetails.name,
         userID: this.signupDetails.userID,
         password: this.signupDetails.password,
         email: this.signupDetails.email,
       };
-      
+
 
       axios.post(`${Config.baseUrl}/auth/signup`, newSignUp)
-                .then((result)=>{
-                    console.log(result);
-                    console.log("1");
-                    setTimeout(() => {
-                this.$router.push("/");
-            }, 1000);
-                })
-                .catch((err) =>{
-                    alert("Give Proper Details")
-                    console.log(err);
-                    console.log("0");
-                })
+        .then((result) => {
+          console.log("1");
+          this.loading = true;
+          setTimeout(() => {
+            this.$toast.success(result.data.message);
+            this.$router.push("/");
+          }, 1000);
+        })
+        .catch((err) => {
+          this.loading = false;
+          this.$toast.error(err.response.data);
+        })
 
       e.preventDefault();
     },
@@ -110,6 +103,7 @@ export default {
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
 }
+
 body {
   min-height: 100vh;
   display: flex;
@@ -117,6 +111,7 @@ body {
   justify-content: center;
   background: #4070f4;
 }
+
 .wrapper {
   margin-top: 50px;
   margin-left: 35%;
@@ -128,6 +123,7 @@ body {
   border-radius: 6px;
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
 }
+
 .wrapper h2 {
   position: relative;
   font-size: 22px;
@@ -138,10 +134,12 @@ body {
 .wrapper form {
   margin-top: 30px;
 }
+
 .wrapper form .input-box {
   height: 52px;
   margin: 18px 0;
 }
+
 form .input-box input {
   height: 100%;
   width: 100%;
@@ -155,20 +153,24 @@ form .input-box input {
   border-radius: 6px;
   transition: all 0.3s ease;
 }
+
 .input-box input:focus,
 .input-box input:valid {
   border-color: #4070f4;
 }
+
 form .policy {
   display: flex;
   align-items: center;
 }
+
 form h3 {
   color: #707070;
   font-size: 14px;
   font-weight: 500;
   margin-left: 10px;
 }
+
 .input-box.button input {
   color: #fff;
   letter-spacing: 1px;
@@ -176,18 +178,22 @@ form h3 {
   background: #4070f4;
   cursor: pointer;
 }
+
 .input-box.button input:hover {
   background: #0e4bf1;
 }
+
 form .text h3 {
   color: #333;
   width: 100%;
   text-align: center;
 }
+
 form .text h3 a {
   color: #4070f4;
   text-decoration: none;
 }
+
 form .text h3 a:hover {
   text-decoration: underline;
 }
